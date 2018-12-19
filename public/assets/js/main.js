e13beff1add6f1969697556ff1c9e2db86d5dd98
John Purtle <purtlejohnmark@gmail.com>
2018-12-19 11:27:51 -0600

    removed console logs

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -13,18 +13,15 @@ var vueInstance = new Vue({
 			return 100
 		},
 		beforeEnter: function(el) {
-			console.log("beforeEnter");
 			el.style.opacity = 0;
 		},
 		enter: function(el, done) {
-			console.log("enter");
 			let delay = el.dataset.index * this.delayedBy();
 			setTimeout(function() {
 				$(el).animate({ opacity: 1 }, 300, done);
 			}, delay);
 		},
 		leave: function(el, done) {
-			console.log("leave");
 			let delay = el.dataset.index * this.delayedBy();
 			setTimeout(function() {
 				$(el).animate({ opacity: 0 }, 300, done);
